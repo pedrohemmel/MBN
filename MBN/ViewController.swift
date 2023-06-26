9412f59bf30d4223a828d44f1124c3c1c510cf80
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2023-06-26 12:13:01 -0300

    Creating project

---
+++ MBN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MBN
+//
+//  Created by Pedro henrique Dias hemmel de oliveira souza on 26/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .green
+    }
+
+
+}
+

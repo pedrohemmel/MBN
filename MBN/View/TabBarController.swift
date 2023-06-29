9f9fd8f1688d9198454537819b2928dc96dc964f
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2023-06-29 10:54:11 -0300

    Creation of tabBarController

---
+++ MBN/View/TabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarController.swift
+//  MBN
+//
+//  Created by Pedro henrique Dias hemmel de oliveira souza on 29/06/23.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

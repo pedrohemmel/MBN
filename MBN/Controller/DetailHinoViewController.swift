bf4278571fa8c1992c8e9a42ebe8e512acb93bd8
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2023-07-05 00:11:26 -0300

    Updates

--- MBN/Controller/DetailHinoViewController.swift
+++ MBN/Controller/DetailHinoViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class DetailHinoViewController: UIViewController {
     
+    var hymn: Hinario? = nil
     let detailView = DetailHinoView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = self.detailView
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Background")!)
+        
+        let favoriteBtn = UIBarButtonItem(image: nil, style: .plain, target: self, action: nil)
+        favoriteBtn.tag = hymn?.hinarioId ?? 0
+        if self.verifyFavoriteHymn(hymn: self.hymn ?? Hinario(hinarioId: .zero, hinarioName: String(), hinarioLyrics: [Paragraph]())) {
+            favoriteBtn.image = UIImage(systemName: "star.fill")
+            favoriteBtn.action = #selector(self.deselectFavoriteHino)
+        } else {
+            favoriteBtn.image = UIImage(systemName: "star")
+            favoriteBtn.action = #selector(self.selectFavoriteHino)
+        }
+        favoriteBtn.tintColor = .white
+        self.navigationItem.rightBarButtonItem = favoriteBtn
     }
 
 }
@@ -24,5 +37,42 @@ extension DetailHinoViewController{
         self.detailView.title.text = hino.hinarioName
         let hinoText = hino.hinarioLyrics.map { $0.lines.joined(separator: "\n") }.joined(separator: "\n\n")
         self.detailView.hino.text = hinoText
+    }
+    
+    func verifyFavoriteHymn(hymn: Hinario) -> Bool {
+        if let favoriteHinario = UserDefaults.standard.array(forKey: "hinario") as? [Int] {
+            if favoriteHinario.contains(where: {$0 == hymn.hinarioId}) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    @objc func selectFavoriteHino(button: UIButton) {
+        if let favoriteHinario = UserDefaults.standard.array(forKey: "hinario") as? [Int] {
+            var newFavoriteHinario = favoriteHinario
+            if !newFavoriteHinario.contains(button.tag) {
+                newFavoriteHinario.append(button.tag)
+                UserDefaults.standard.set(newFavoriteHinario, forKey: "hinario")
+                self.navigationItem.rightBarButtonItem?.image = UIImage(systemName: "star.fill")
+                self.navigationItem.rightBarButtonItem?.action = #selector(self.deselectFavoriteHino)
+            }
+        } else {
+            UserDefaults.standard.setValue([button.tag], forKey: "hinario")
+            self.navigationItem.rightBarButtonItem?.image = UIImage(systemName: "star.fill")
+            self.navigationItem.rightBarButtonItem?.action = #selector(self.deselectFavoriteHino)
+        }
+    }
+    
+    @objc func deselectFavoriteHino(button: UIButton) {
+        if let favoriteHinario = UserDefaults.standard.array(forKey: "hinario") as? [Int] {
+            var newFavoriteHinario = favoriteHinario
+            if newFavoriteHinario.contains(button.tag) {
+                newFavoriteHinario.remove(at: newFavoriteHinario.firstIndex(where: {$0 == button.tag}) ?? 0)
+                UserDefaults.standard.set(newFavoriteHinario, forKey: "hinario")
+                self.navigationItem.rightBarButtonItem?.image = UIImage(systemName: "star")
+                self.navigationItem.rightBarButtonItem?.action = #selector(self.selectFavoriteHino)
+            }
+        }
     }
 }
--- MBN/Controller/HomeViewController.swift
+++ MBN/Controller/HomeViewController.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    private var hinario = [Hinario]()
-    private var detailHinoViewController = DetailHinoViewController()
     lazy var homeView = HomeView()
     
     override func loadView() {
@@ -38,76 +36,27 @@ extension HomeViewController: HinoDelegate{
         let hinoVC = DetailHinoViewController()
         hinoVC.setup(hino)
         hinoVC.title = "Hino"
+        hinoVC.hymn = hino
         
         let backBtn = UIBarButtonItem(title: "Voltar", style: .plain, target: self, action: #selector(back))
         backBtn.tintColor = .white
         hinoVC.navigationItem.leftBarButtonItem = backBtn
         
-        let favoriteBtn = UIBarButtonItem(image: nil, style: .plain, target: self, action: nil)
-        favoriteBtn.tag = hino.hinarioId
-        if verifyFavoriteHymn(hymn: hino) {
-            favoriteBtn.image = UIImage(systemName: "star.fill")
-            favoriteBtn.action = #selector(self.deselectFavoriteHino)
-        } else {
-            favoriteBtn.image = UIImage(systemName: "star")
-            favoriteBtn.action = #selector(self.selectFavoriteHino)
-        }
-        favoriteBtn.tintColor = .white
-        hinoVC.navigationItem.rightBarButtonItem = favoriteBtn
-        
-        self.detailHinoViewController = hinoVC
-        
-        self.navigationController?.pushViewController(self.detailHinoViewController, animated: true)
+        self.navigationController?.pushViewController(hinoVC, animated: true)
     }
 }
 
 extension HomeViewController {
     func setup(_ list: [Hinario], searchBarDelegate: SearchBarDelegate){
-        self.hinario = list
         self.homeView.hinario.setup(list, self)
         self.homeView.search.searchBarDelegate = searchBarDelegate
-    }
-    
-    func verifyFavoriteHymn(hymn: Hinario) -> Bool {
-        if let favoriteHinario = UserDefaults.standard.array(forKey: "hinario") as? [Int] {
-            if favoriteHinario.contains(where: {$0 == hymn.hinarioId}) {
-                return true
-            }
-        }
-        return false
     }
     
     @objc func back() {
         self.navigationController?.popViewController(animated: true)
     }
     
-    @objc func selectFavoriteHino(button: UIButton) {
-        if let favoriteHinario = UserDefaults.standard.array(forKey: "hinario") as? [Int] {
-            var newFavoriteHinario = favoriteHinario
-            if !newFavoriteHinario.contains(button.tag) {
-                newFavoriteHinario.append(button.tag)
-                UserDefaults.standard.set(newFavoriteHinario, forKey: "hinario")
-                self.detailHinoViewController.navigationItem.rightBarButtonItem?.image = UIImage(systemName: "star.fill")
-                self.detailHinoViewController.navigationItem.rightBarButtonItem?.action = #selector(self.deselectFavoriteHino)
-            }
-        } else {
-            UserDefaults.standard.setValue([button.tag], forKey: "hinario")
-            self.detailHinoViewController.navigationItem.rightBarButtonItem?.image = UIImage(systemName: "star.fill")
-            self.detailHinoViewController.navigationItem.rightBarButtonItem?.action = #selector(self.deselectFavoriteHino)
-        }
-    }
     
-    @objc func deselectFavoriteHino(button: UIButton) {
-        if let favoriteHinario = UserDefaults.standard.array(forKey: "hinario") as? [Int] {
-            var newFavoriteHinario = favoriteHinario
-            if newFavoriteHinario.contains(button.tag) {
-                newFavoriteHinario.remove(at: newFavoriteHinario.firstIndex(where: {$0 == button.tag}) ?? 0)
-                UserDefaults.standard.set(newFavoriteHinario, forKey: "hinario")
-                self.detailHinoViewController.navigationItem.rightBarButtonItem?.image = UIImage(systemName: "star")
-                self.detailHinoViewController.navigationItem.rightBarButtonItem?.action = #selector(self.selectFavoriteHino)
-            }
-        }
-    }
 }
 
 
